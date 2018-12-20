37a8fb8c848ca0348c77194d971f7205f4299b66
Francesco Rivola <francesco.rivola@gmail.com>
2018-12-20 20:38:39 +0100

    added eslint and babel configuration

---
+++ build/index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+const hello = 'world';
+exports.default = hello;
---
+++ src/index.js
@@ -0,0 +1,2 @@
+const hello = 'world';
+export default hello;
